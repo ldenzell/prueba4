fb2d65c05b8e6c56c5e4f6860ad97be7c47ac77d
ldenzell <danzel.drd.20@gmail.com>
2019-09-10 01:38:32 +0200

    inicio del proyecto

---
+++ loginn.js
@@ -0,0 +1,27 @@
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "test";
+
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+if (!$conn) 
+{
+	die("No hay conexión: ".mysqli_connect_error());
+}
+
+$nombre = $_POST["txtusuario"];
+$pass = $_POST["txtpassword"];
+
+$query = mysqli_query($conn,"SELECT * FROM login WHERE usuario = '".$nombre."' and password = '".$pass."'");
+$nr = mysqli_num_rows($query);
+
+if($nr == 1)
+{
+	//header("Location: pagina.html")
+	echo "Bienvenido:" .$nombre;
+}
+else if ($nr == 0) 
+{
+	header("Location: login.html");
+	//echo "No ingreso"; 
+}
